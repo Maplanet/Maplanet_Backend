d5ee52f831d6f814708cb823ab0ebb655273a96e
ystar5008@naver.com <ystar5008@naver.com>
2024-02-17 17:49:26 +0900

    cicd테스트

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,10 @@ module.exports = {
     apps: [{
         script: 'dist/main.js',
         name: 'maplanet-nest-app',
-        exec_mode: 'cluster',
-        instances: 2,
+        exec_mode: 'fork',
+        //instances: 'max',
+        error_file: 'logs/error.log',
+        out_file: 'logs/out.log',
+        autorestart: true,
     }]
 };
