6b2d47bdfe9f117dfa546db8300b929d1e5138bf
ParkBrianJunSoo <junparkstar@gmail.com>
2024-02-17 18:26:47 +0900

    fix: pm2 monit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,6 @@ module.exports = {
         name: 'maplanet-nest-app',
         exec_mode: 'fork',
         //instances: 'max',
-        error_file: './error.log',
-        out_file: './logs/out.log',
         autorestart: true,
     }]
 };
