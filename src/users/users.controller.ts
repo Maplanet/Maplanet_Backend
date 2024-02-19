7bd1f60866eea20301528e95d0b726a41a70eb10
ParkBrianJunSoo <junparkstar@gmail.com>
2024-02-19 15:57:50 +0900

    feat: Discord login

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UsersService } from './users.service';
+// import { DiscordAuthGuard } from '../auth/discord-auth.guard'
 
-@Controller('users')
+@ApiTags('USERS')
+@Controller('/users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  // @Get('/discord/login')
+  // @UseGuards(DiscordAuthGuard)
+  // @ApiOperation({
+  //   summary: '디스코드 로그인',
+  //   description: '디스코드 로그인'
+  // })
+  // async discordLogin(@Req() req: Request) {
+    
+  // }
+
+  // @Get('discord/callback')
+  // @UseGuards(DiscordAuthGuard)
+  // async discordLoginCallback(@Req() req: Request, @Res() res: Response) {
+  //   res.redirect('/')
+  // }
 }
