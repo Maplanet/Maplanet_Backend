bf78597fcdbec1a8f23f0c1a1bd8bbbe3d4430f3
ParkBrianJunSoo <junparkstar@gmail.com>
2024-02-20 01:37:09 +0900

    fix: Discord login get access token & refresh token

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,23 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from './jwt.strategy';
-import { JwtModule } from '@nestjs/jwt';
-import { DiscordStrategy } from './discord.strategy';
-import { AppModule } from 'src/app.module';
 
 @Module({
-  imports: [
-    PassportModule,
-    AppModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1d' },
-    }),
-  ],
   controllers: [UsersController],
-  providers: [UsersService, DiscordStrategy, JwtStrategy],
+  providers: [UsersService],
   exports: [UsersService],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,54 @@
-import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Users } from './entities/users.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { ApiExtraModels } from '@nestjs/swagger';
+import axios from 'axios';
+import { response } from 'express';
 
 @Injectable()
 export class UsersService {
-    constructor(
-        private readonly jwtService: JwtService,
-        @InjectRepository(Users)
-        private usersRepository: Repository<Users>
-        ) {}
+  constructor(
+    // @InjectRepository(Users)
+        // private usersRepository: Repository<Users>
+  ) {}
 
-  async login(user: any): Promise<string> {
-    const payload = { username: user.username, sub: user.id };
-    return this.jwtService.sign(payload);
+  async discordLogin(code) {
+    const requestToken = code;
+    console.log(code)
+    let tokenResponse, userResponse;
+    try {
+      tokenResponse = await axios({
+          method: 'post',
+          url: `https://discord.com/api/oauth2/token`,
+          data: {
+              client_id: process.env.DISCORD_CLIENT_ID,
+              client_secret: process.env.DISCORD_CLIENT_SECRET,
+              grant_type: 'authorization_code',
+              code: requestToken,
+              redirect_uri: 'http://localhost:3000/discord/login',
+              scope: 'identify, email',
+          },
+          headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+          }
+      });
+
+      const accessToken = tokenResponse.data.access_token;
+
+      // Use the access token to fetch the user's data from Discord API
+      userResponse = await axios.get('https://discordapp.com/api/users/@me', {
+          headers: {
+              authorization: `Bearer ${accessToken}`,
+          },
+      });
+      console.log("accessToken",accessToken)
+      console.log("tokenResponse",tokenResponse)
+
+  } catch (error) {
+      console.error(`Error in getting token or user data from Discord API: ${error.message}`);
+  }
+  console.log(userResponse.data); // This logs the user's data
+    
   }
 }
