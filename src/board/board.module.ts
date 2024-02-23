8f8bd55ecb609b55c0496470916e8cffd4453287
ParkBrianJunSoo <junparkstar@gmail.com>
2024-02-24 00:57:00 +0900

    fix: Board1, board1 detail, board1 search complete

--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -6,9 +6,10 @@ import { Board } from './entities/board.entity';
 import { UsersModule } from 'src/users/users.module';
 import { UsersService } from 'src/users/users.service';
 import { AuthGuard } from 'src/common/auth.guard';
+import { Users } from 'src/users/entities/users.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Board]),UsersModule],
+  imports: [TypeOrmModule.forFeature([Board, Users]),UsersModule],
   controllers: [BoardController],
   providers: [BoardService],
 })
